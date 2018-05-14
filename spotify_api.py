ad0c062677c959eab9828569960c8064f1a52bd8
mathiasjepsen <mathias@wpsnet.com>
2018-05-14 08:26:14 +0200

    Formatting and renaming of functions/variables

--- spotify_api.py
+++ spotify_api.py
@@ -2,24 +2,29 @@ import requests
 import json
 import api_key
 
+global user_id
+global playlist_name
+global playlist_description
 
-def create_playlist():
-    user_id = "serenitymusic97"
+def create_playlist(user_id, playlist_name, playlist_description):
+    user_id = user_id
+    playlist_name = playlist_name
+    playlist_description = playlist_description
     url_create_playlist = f"https://api.spotify.com/v1/users/{user_id}/playlists"
     headers = {"Accept": "application/json",
                "Content-Type": "application/json",
                "Authorization": f"Bearer {api_key.SPOTIFY_TOKEN}"}
     body = {
-        "name": "Test playlist",
-        "description": "Test desc"
-}
+        "name": playlist_name,
+        "description": playlist_description
+    }
     r = requests.post(url_create_playlist, headers=headers, data=json.dumps(body))
     results = r.json()
     print(results)
+    # playlist_id = 
 
-def add_to_playlist(uris):
-    user_id = "serenitymusic97"
-    playlist_id = "4nhErVvTyR25yVO0oKV3OT"
+
+def add_to_playlist(uris, playlist_id):
     url = f"https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks"
     headers = {"Accept": "application/json",
                "Content-Type": "application/json",
--- youtube_playlist.py
+++ youtube_playlist.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 import requests
 import json
 import api_key
-import spotify_api_calls
+import spotify_api
 
 playlist_link = "https://www.youtube.com/playlist?list=PLBILG-T6e7eDeszgpd2qZTAcRIHEvn4ES"
 browser = webdriver.Chrome()
@@ -27,55 +27,53 @@ sleep(2)
 
 titles = []
 for _ in tqdm(range(int(num_of_videos))):
-    if not browser.find_elements_by_xpath('//*[@class="reason style-scope ytd-player-error-message-renderer"]'):
+    if not browser.find_elements_by_xpath(
+            '//*[@class="reason style-scope \
+            ytd-player-error-message-renderer"]'):
         title = pytube.YouTube(browser.current_url).title
         titles.append(title)
         next_button = browser.find_element_by_xpath(
             '//*[@class="ytp-next-button ytp-button"]')
-        #browser.execute_script("arguments[0].click();", next_button)
-        next_button.click()
+        browser.execute_script("arguments[0].click();", next_button)
+        #next_button.click()
     else:
         title = browser.find_element_by_xpath(
-            '//*[@class="style-scope ytd-video-primary-info-renderer"]').text.splitlines()[0]
+            '//*[@class="style-scope ytd-video-primary-info-renderer"]') \
+            .text.splitlines()[0]
         titles.append(title)
         sleep(5)
 
 
-titles_to_URL = []
-for title in titles:
-    title_split = title.split()
-    title_url = ""
-    for idx, word in enumerate(title_split):
-        if(idx != len(title_split) - 1) :
-            title_url += word + "%20"
-        else :
-            title_url += word
-    titles_to_URL.append(title_url)     
-    print(titles_to_URL)
+titles_to_URL = [title.replace(" ", "%20") for title in titles]
+
+
+# titles_to_URL = []
+# for title in tqdm(titles):
+#     title_split = title.split()
+#     title_url = ""
+#     for idx, word in enumerate(title_split):
+#         if(idx != len(title_split) - 1) :
+#             title_url += word + "%20"
+#         else :
+#             title_url += word
+#     titles_to_URL.append(title_url)     
 
 
 uris = []
 for url_title in titles_to_URL:
     url = f"https://api.spotify.com/v1/search?q={url_title}&type=track"
+
     headers = {"Accept": "application/json",
-            "Content-Type": "application/json",
-            f"Authorization": "Bearer {api_key.SPOTIFY_KEY}"}
+               "Content-Type": "application/json",
+               f"Authorization": "Bearer {api_key.SPOTIFY_TOKEN}"}
 
     r = requests.get(url, headers=headers)
     results = r.json()
     print(results)
     uris.append(results["tracks"]["items"][0]["uri"])
 
-spotify_api_calls.create_playlist()
-spotify_api_calls.add_to_playlist(uris)
 
-
-# for id in ids:
-#     url = f"https://api.spotify.com/v1/tracks/{id}"
-#     query = {"Accept": "application/json",
-#             "Content-Type": "application/json",
-#             "Authorization": "Bearer "}
-
-#     r = requests.get(url, headers=query)
-#     results = r.json()
-#     print(results)
+spotify_api.create_playlist("serenitymusic97", 
+                            "Awesome Test Playlist", 
+                            "Boring description")
+spotify_api.add_to_playlist(uris)
