e78a318fb4da72453ad4efe364d78d11258fc544
lovrobiljeskovic <biljeskovic.lovro@gmail.com>
2018-05-17 16:48:33 +0200

    created a flow where we search for a song and based of off the artist name and song title, the lyrics get printed out

--- spotify_api.py
+++ spotify_api.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import api_key
+from bs4 import BeautifulSoup
 
 
 def create_playlist(ID, playlist_name, playlist_description):
@@ -67,8 +68,36 @@ def search(q, typeSearch):
     r = requests.get(url, headers = headers,
                 params = params)
     data = r.json()
-    print(data)
+    tracks = data["tracks"]["items"]
+    for track in tracks:
+        dict = {
+            "song_title": track["name"],
+            "artist_name": track["artists"][0]["name"]
+            }
+        # print(track["name"])
+        # print("------------------------------------------")
+        # print(track["artists"][0]["name"])
+        print(dict)
+    return dict
 
+
+def request_song_info(song_title, artist_name):
+    base_url = 'https://api.genius.com'
+    headers = {'Authorization': 'Bearer ' + 'ccHDzjsPAnwWcOAo2fS5Kb5cH9BFLqsryi5r0a6p3flgb_B3g8qPguO68A3NZTZJ'}
+    search_url = base_url + '/search'
+    data = {'q': song_title + ' ' + artist_name}
+    response = requests.get(search_url, data=data, headers=headers)
+    results = response.json()
+    #print(results)
+    return response
+
+def scrap_song_url(url):
+    page = requests.get(url)
+    html = BeautifulSoup(page.text, 'html.parser')
+    [h.extract() for h in html('script')]
+    lyrics = html.find('div', class_='lyrics').get_text()
+
+    return lyrics
 
 
 def readFromConsole():
@@ -84,6 +113,27 @@ def readFromConsole():
     input_q = input("provide a name of an item you are searching for:")
     input_type = input("provide a category album/artist/playlist/track:")
     search(input_q, input_type)
+    input_artist = input("Choose an artist:")
+    response = request_song_info(input_q, input_artist)
+    json = response.json()
+    remote_song_info = None
+
+    for hit in json['response']['hits']:
+        if input_artist.lower() in hit['result']['primary_artist']['name'].lower():
+            remote_song_info = hit
+            break
+
+    if remote_song_info:
+        song_url = remote_song_info['result']['url']
+        lyrics = scrap_song_url(song_url)
+
+
+    print(lyrics)
+
+
+
+
+
 
 
 readFromConsole()
